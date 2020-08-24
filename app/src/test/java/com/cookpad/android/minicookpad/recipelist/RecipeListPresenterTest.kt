9ee7d205204520621ba31309b6a7cda10e9bd876
kmgk <hiragi.kame@gmail.com>
2020-08-24 22:04:45 +0900

    02 RecipeListPresenter.onRecipeListRequestedSuccess() が成功したときのテストを作成

--- app/src/test/java/com/cookpad/android/minicookpad/recipelist/RecipeListPresenterTest.kt
+++ app/src/test/java/com/cookpad/android/minicookpad/recipelist/RecipeListPresenterTest.kt
@@ -1,2 +1,49 @@
 package com.cookpad.android.minicookpad.recipelist
 
+import com.google.common.truth.Truth.assertThat
+import com.nhaarman.mockitokotlin2.*
+import org.junit.Before
+import org.junit.Test
+
+class RecipeListPresenterTest {
+    lateinit var view: RecipeListContract.View
+    lateinit var interactor: RecipeListContract.Interactor
+    lateinit var routing: RecipeListContract.Routing
+    lateinit var presenter: RecipeListPresenter
+
+    @Before
+    fun setup() {
+        view = mock()
+        routing = mock()
+        interactor = mock()
+        presenter = RecipeListPresenter(view, interactor, routing)
+    }
+
+    @Test
+    fun verifyOnRecipeListRequestedSuccess() {
+        // given
+        val recipeList = listOf(
+            RecipeListContract.Recipe(
+                id = "xxx",
+                title = "おいしいきゅうりの塩もみ",
+                imagePath = "images/recipe.png",
+                steps = "きゅうりを切る、塩をまく、もむ",
+                authorName = "クックパッド味"
+            )
+        )
+        whenever(interactor.fetchRecipeList(any(), any())).then {
+            (it.arguments[0] as (List<RecipeListContract.Recipe>) -> Unit).invoke(recipeList)
+        }
+
+        // when
+        presenter.onRecipeListRequested()
+
+        // then
+        val argumentCaptor = argumentCaptor<List<RecipeListContract.Recipe>>()
+        verify(interactor).fetchRecipeList(any(), any())
+        verify(view).renderRecipeList(argumentCaptor.capture())
+        argumentCaptor.firstValue.also {
+            assertThat(it).isEqualTo(recipeList)
+        }
+    }
+}
